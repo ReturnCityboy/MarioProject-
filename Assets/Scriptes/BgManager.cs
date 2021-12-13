0283cb76a87a34ca9634acf67b24fae0b86718a0
cityboy <gooxgoo@naver.com>
2021-12-13 11:32:08 +0900

    적 카운트 에러 발생

--- Assets/Scriptes/BgManager.cs
+++ Assets/Scriptes/BgManager.cs
@@ -26,24 +26,23 @@ public class BgManager : MonoBehaviour
     public float playerRange = 12f;
     public GameObject player;
 
+    [Header("CLOUD")]
     public GameObject cloudPrefabs;
-
-    public GameObject enemyPrefabs;
-    public List<GameObject> enemyList = new List<GameObject>();
-
     public int cloudNum;
-    public int enemyNum;
     public int cloudNumMax = 3;
-    public int enemyNumMax = 3;
     public float cloudSpawnTime = 1f;
     public float cloudTime;//현재의 시간
-    public float enemySpawnTime = 4f;
+    public List<Vector2> cloudnodes = new List<Vector2>();
+
+    [Header("ENEMY")]
+    public GameObject enemyPrefabs;
+    public int enemyNum;
+    public int enemyNumMax = 3;
+    public float enemySpawnTime = 8f;
     public float enemyTime;//현재의 시간
-
-
-    public List<GameObject> enemyoff = new List<GameObject>();
-
-    public List<Vector2> nodes = new List<Vector2>();
+    public List<GameObject> enemyList = new List<GameObject>();
+    public List<GameObject> enemyOff = new List<GameObject>();
+    public List<Vector2> enemyOn = new List<Vector2>();
     public List<Vector2> enemynodes = new List<Vector2>();
 
 
@@ -71,47 +70,41 @@ public class BgManager : MonoBehaviour
             {
                 //Debug.Log("구름수" + cloudNum);
                 GameObject c = Instantiate(cloudPrefabs);
-                int n = Random.Range(0, nodes.Count);//(0번 노드,노드갯수)
-                c.transform.position = new Vector2(nodes[n].x, nodes[n].y);
+                int n = Random.Range(0, cloudnodes.Count);//(0번 노드,노드갯수)
+                c.transform.position = new Vector2(cloudnodes[n].x, cloudnodes[n].y);
                 cloudTime = 0;
                 cloudNum++;
             }
-
-
-
         }
 
         OffEnemy();
 
-        //enemyTime += Time.deltaTime;
-        if (enemyNum < enemyNumMax)
+        enemyTime += Time.deltaTime;
+        if (enemyTime > enemySpawnTime)
         {
-            //GameObject c = Instantiate(enemyPrefabs);
-            //int n = Random.Range(0, enemynodes.Count);
-            //c.transform.position = new Vector2(enemynodes[n].x, enemynodes[n].y);
-
-
-
-            //EnemyList[0]의 몇번째 적인지를 알려줘야한다
-
-            for (int i = 0; i < enemyList.Count; i++)
+            if (enemyNum < enemyNumMax)
             {
-                OnObj(enemyList[i], enemynodes);
-            }
-            enemyTime = 0;
-            enemyNum++;
-        }
+                Debug.Log("적리스폰");
 
+
+                for (int i = 0; i < enemyList.Count; i++)
+                {
+                    OnEnemy(enemyList[i]);
+                    Debug.Log(enemyList[i]);
+                }
+                enemyTime = 0;
+                enemyNum++;
+            }
+
+        }
     }
 
     void enemyInit()//정신만 생성
     {
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < enemyNumMax; i++)
         {
-
             GameObject _enemy = Instantiate(enemyPrefabs) as GameObject;
-            enemyList.Add(_enemy);//enemylist에 추가
-
+            enemyList.Add(_enemy);
             _enemy.SetActive(false);
         }
     }
@@ -120,55 +113,90 @@ public class BgManager : MonoBehaviour
     public void DieEnemy(GameObject obj)
     {
         obj.SetActive(false);//가리고
-        enemyoff.Add(obj);//회수
-        enemyList.Remove(obj);
+        //enemyOff.Add(obj);//회수
+        //enemyList.Remove(obj);
+        
+        //Todo: 12/13  문제발견 적카운트가 무한 발생
+        // 숙제 카운트 1 문제가 무엇인지 2 해결방법 주석
         enemyNum--;//출고갯수처리
     }
     private void OffEnemy()
     {
-
-        //만약에 적의x값이 플레이어레인지*2한것보다 작다면 DieEnemy(enemyList[i])처리 (업데이트)
-        for (int i = 0; i < enemyList.Count; i++)//에너미 리스트
+        for (int i = 0; i < enemyList.Count; i++)
         {
-
             if (enemyList[i].transform.position.x < player.transform.position.x - playerRange)
             {
                 Debug.Log("적list" + enemyList[i]);
                 // enemyList.Remove(enemyList[i]);
                 DieEnemy(enemyList[i]);
-
             }
+
+            //적이 새로 생성안되는 문제
+            //에너미 갯수가 작아진건 디버그
+            //OnObj 디버그는 나오는데 이미지가 켜지질않는다 켜질 노드를 찾지 못하는듯
+            //enemyList가 0인데(여기까지 디버그) 재생성이 되지 않는다
+            //만약 enemyList가 0 이라면
         }
 
     }
 
-    private void OnObj(GameObject obj, List<Vector2> listV)//listV모든랜덤리스트
+    private void OnEnemy(GameObject obj)
     {
-        //제품의 존재, 진열위치     
-        obj.SetActive(true);//오브젝트를 켜주고
-                            //int n = Random.Range(0, listV.Count);//(0번 노드,노드갯수)랜덤위치
-                            //obj.transform.position = new Vector2(listV[n].x, listV[n].y);//그위치로 보내기
-                            //카메라 안(플레이어)의 노드중 랜덤 생성
 
-        float playerMin = player.transform.position.x;//플레이어위치
-        float playerMax = player.transform.position.x + playerRange;//플레이어의 범위 = 플레이어 x값 +_특정값  = playerRange
+        obj.SetActive(true);
 
-        List<Vector2> _listV = new List<Vector2>();//_listV카메라속(playerMin,Max) 랜덤리스트
-        for (int i = 0; i < listV.Count - 1; i++)
+        float playerMin = player.transform.position.x;
+        float playerMax = player.transform.position.x + playerRange;
+
+        List<Vector2> _listV = new List<Vector2>();
+
+
+        for (int i = 0; i < enemynodes.Count - 1; i++)
         {
-            if (playerMin < listV[i].x && playerMax > listV[i].x)//플레이어보다 앞에있는(레인지)
+            if (enemynodes != null)
             {
-                _listV.Add(listV[i]);//_리스트에 추가
+                if (playerMin < enemynodes[i].x && playerMax > enemynodes[i].x)
+                {
+                    for (int e = 0; e < enemyList.Count; e++)
+                    {
+                        if (enemyList != null)
+                        {
+                            if (enemyList[e].transform.position.x != enemynodes[i].x)
+                            {
+                                _listV.Add(enemynodes[i]);
+                            }
+                        }
+                    }
+                }
             }
+            if (_listV.Count > 0)
+            {
+                int _n = (_listV.Count) - 1;
+                int n = Random.Range(0, _n);
+                Vector2 randomVector = new Vector2(_listV[n].x, _listV[n].y);
 
-            int n = Random.Range(0, _listV.Count - 1);//(0번 노드,인덱스 최대값)
-            obj.transform.position = new Vector2(_listV[n].x, _listV[n].y); //에러
+                if (!enemyOn.Contains(randomVector))
+                {
+                    obj.transform.position = randomVector;
+                    enemyOn.Add(randomVector);
+                }
+                else
+                {
+                    Debug.Log("랜덤 돌려서 빈곳에 에너미 위치를 바꾼다=반복적으로사용해서조건이맞을때까지반복하고위치가바뀐다면 멈춰라 반복을"); 
+                
+                    
+                }
+            }
+            else
+            {
+                Debug.Log("랜덤 리스트 카운트가 0보다 작거나 같다.");
+            }
         }
-
     }
-
-    //버벗이 죽으면 숫자는 카운트된다  새로생성되는 적이 보이지 않는다
-    //버섯이 죽는데 숮ㅅ자 3이 유지된다
-    //카메라 밖으로 나가면 적은 죽는다 버그발생
-    //하이락키에서 에너미 오브젝트 삭제 인스펙터에서는 3존재
 }
+
+
+
+
+
+
